7ee7e506f7179981913e530fc25ffa2d69a14bc8
Josh-6 <Joshuabarpna@gmail.com>
2024-09-19 13:05:36 -0700

    Able to get the largest numbers

--- fib.c
+++ fib.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+int64_t iterative(int s)
+{
+   int64_t  prev_num = 0;
+   int64_t  saved_num = 1;
+   int64_t  current_num;
+
+   for(int i = 2; i < s; i++)
+   {
+      current_num = prev_num + saved_num;
+      prev_num = saved_num;
+      saved_num = current_num;
+   }
+   return current_num;
+}
+
+int64_t recursive(int n)
+{
+   if(n == 1)
+   {
+      return 0;
+   }
+   else if(n == 2)
+   {
+      return 1;
+   }
+   else
+   {
+      return (recursive(n - 1) + recursive(n-2));
+   }
+}
+
+int main(int argc, char* argv[]) 
+{
+
+   int user_number = atoi(argv[1]);
+   
+   int64_t fibo;
+   
+   if (strcmp(argv[2], "i") == 0)
+   {
+      fibo = iterative(user_number);
+   }
+   else
+   {
+      //printf("Running Recursive\n");
+      fibo = recursive(user_number);
+   }
+   printf("%" PRIi64 "\n", fibo);
+   
    return 0;
 }
